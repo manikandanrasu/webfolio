ffa8f57a51728fa89d7486207e0cb492504c48f4
manikandanrasu <manikandanrasubj@gmail.com>
2025-05-07 08:38:51 +0530

    Initial commit for portfolio

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import { Socials } from "@/constants";
+import Image from "next/image";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20">
+      <div className="flex flex-row gap-3 items-center">
+        <h1 className="text-white text-[25px] font-semibold">
+          M{" "}
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
+            {" "}
+            K{" "}
+          </span>
+        </h1>
+      </div>
+
+      <div className="flex flex-row gap-5 mb-2">
+        {Socials.map((social) => (
+          <a
+            key={social.name}
+            href={social.href}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+
+            <Image
+              src={social.src}
+              alt={social.name}
+              width={28}
+              height={28}
+            />
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/constants/index.ts
@@ -0,0 +1,117 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: "80",
+    height: "80",
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: "80",
+    height: "80",
+  },
+  {
+    name: "Python",
+    Image: "/Python.webp",
+    width: "80",
+    height: "80",
+  },
+  {
+    name: "Flask",
+    Image: "/Flask.webp",
+    width: "80",
+    height: "80",
+  },
+  {
+    name: "Django",
+    Image: "/django.webp",
+    width: "80",
+    height: "80",
+  },
+  {
+    name: "PostgreSQL",
+    Image: "/PostgresSQL.webp",
+    width: "80",
+    height: "80",
+  },
+  {
+    name: "MySQL",
+    Image: "/mysql.png",
+    width: "80",
+    height: "80",
+  },
+  {
+    name: "Redis",
+    Image: "/Redis_1.webp",
+    width: "80",
+    height: "80",
+  },
+  {
+    name: "AWS",
+    Image: "/AWS_1.webp",
+    width: "80",
+    height: "80",
+  },
+];
+
+export const Socials = [
+    {
+      name: "Gmail",
+      src: "/gmail.svg",
+      href: "mailto:manikandanrasubj@gmail.com",
+    },
+    {
+      name: "Github",
+      src: "/github.svg",
+      href: "https://github.com/manikandanrasu",
+    },
+    {
+      name: "LinkedIn",
+      src: "/LinkedIn.svg",
+      href: "https://www.linkedin.com/in/mani-r",
+    },
+  ];
+
+  export const Projects = [
+    {
+      title: "Stock data visualization",
+      text: "the dectiosjdnc dcdjcb jwwdwdjin sksownnxjnx sjssxnjxxnsx jasbdxjsb sxbds skx jbcdxsjjc qhd",
+      src: "/financial-data.jpg",
+    },
+    {
+      title: "Spotify-clone",
+      text: "the dectiosjdnc dcdjcb jwwdwdjin sksownnxjnx sjssxnjxxnsx jasbdxjsb sxbds skx jbcdxsjjc qhd",
+      src: "/spotify-clone.jpg",
+    },
+    {
+      title: "Portfolio website",
+      text: "the dectiosjdnc dcdjcb jwwdwdjin sksownnxjnx sjssxnjxxnsx jasbdxjsb sxbds skx jbcdxsjjc qhd",
+      src: "/main-bg.webp",
+    },
+  ];
+
+  export const NavLinks = [
+    {
+      name: "/",
+      icon: RxHome,
+      link: "/",
+    },
+    {
+      name: "/my-skills",
+      icon: RxPerson,
+      link: "/my-skills",
+    },
+    {
+      name: "/my-projects",
+      icon: RxDashboard,
+      link: "/my-projects",
+    },
+    {
+      name: "/contact-me",
+      icon: RxClipboard,
+      link: "/contact-me",
+    },
+  ];
