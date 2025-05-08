0f37126bae535f3e23493a85a648343049824596
manikandanrasu <manikandanrasubj@gmail.com>
2025-05-08 11:19:46 +0530

    deployment script error resolved

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig } */
 
-const nextConfig: NextConfig = {
-  output: "export",
-};
+const nextConfig = {
+  output: 'export',
+}
 
 export default nextConfig;
