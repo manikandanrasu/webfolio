9abd4cf70e9a52941d263b216f561ee804d07191
manikandanrasu <manikandanrasubj@gmail.com>
2025-07-09 09:53:11 +0530

    remove unused variable

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,8 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Socials } from "@/constants";
 import Image from "next/image"; 
 
 const Navbar = () => {
-  const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrolled(window.scrollY > 10);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <div className="fixed top-0 left-0 z-50 w-full h-[100px] flex justify-between items-center px-10 md:px-20 transition-all duration-300 bg[#fefadf]/60 backdrop-blur">
       <div className="text-black text-normal font-[montserrat] font-medium tracking-wider">mani.codes</div>
